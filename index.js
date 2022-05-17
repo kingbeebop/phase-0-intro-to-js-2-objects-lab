06cef046833f5431e7f006a7914eec7cdb4fd7e4
Brendan Ward <kingbeebop@gmail.com>
2022-05-17 10:06:57 -0400

    commit tree 1b4f0fdea3374e7d1a3889f2216206da6e044b85

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "test",
+    streetAddress: "123 Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const employeeCopy = {...obj};
+    delete employeeCopy[key];
+    return employeeCopy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
+
